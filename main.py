6c8e121ee9928b45ffa725ecbf1e449cb7fc6b71
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-05-07 20:48:39 -0100

    feat: setup database connector

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main() -> None:
-    print("Hello World")
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
+from database.db_connector import SQLite_Connector
 
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+api = Api(app)
+
+class Pedrada(Resource):
+    def get(self):
+        return "Pedrada Flask RestFull API"
+    
+class PedradaRestAPI():
+    @staticmethod
+    def start() -> None:
+        api.add_resource(Pedrada, '/')
+        app.run(debug=True)
 
 if __name__ == "__main__":
-    main()
+    PedradaRestAPI.start()
